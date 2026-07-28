756fbe64e731628ef8459263ea1a695be3633f3b
Tom <144795071+tmslpm@users.noreply.github.com>
2026-07-28 02:09:06 +0200

    userscript setup

---
+++ userscripts/cdn/ping.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        test
+// @namespace   tmslpm.test
+// @icon        https://github.githubassets.com/favicons/favicon-dark.svg
+// @version     1.0.0
+// @match       https://www.example.com/?test*
+// @grant       none
+// @author      -
+// @description
+// ==/UserScript==
+alert("ok userscript test")
